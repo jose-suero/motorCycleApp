a682414c978024e81693a2a2fc9eebfbf02dce1e
Jose Suero <jose.suero@gmail.com>
2017-01-20 22:14:57 -0400

    Commit from mi casa

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-var motorcycleApp = angular.module('motorcycleApp', []);
+var motorcycleApp = angular.module('motorcycleApp', ['ngRoute']);
+
+motorcycleApp.config(function($routeProvider, $locationProvider) {
+	debugger;
+	$routeProvider.when('/motorcyle/:motoId', {
+		templateUrl: 'index.html',
+		controller: motorcycleController
+	})
+});
--- js/controllers.js
+++ js/controllers.js
@@ -7,6 +7,7 @@ var motorcycleAppControllers = angular.module('motorcycleApp', []);
 motorcycleAppControllers.controller('motorcycleAppController', ['$scope', '$http', function($scope, $http) {
 	$http.get('motorcycles/motorcycles.json')
 	     .then(function(res){
-	     	alert(JSON.stringify(res.data));
+	     	//motorcycle data received.
+	     	$scope.motorcycles = res.data;
 	     });
 }]);
